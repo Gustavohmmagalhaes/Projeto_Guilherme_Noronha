e289bc316d7a9bced983af60b079d02acbb5c055
AgostiniGuilherme <guilhermenagostini@gmail.com>
2023-10-03 17:09:36 -0300

    Adição da class D com os métodos MD1() e MD2()

---
+++ D.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class D {
+private:
+    int D1;
+    double D2;
+
+public:
+
+    int getD1() {
+        return D1;
+    }
+
+    void setD1(int d1) {
+        D1 = d1;
+    }
+
+    double getD2() {
+        return D2;
+    }
+
+    void setD2(double d2) {
+        D2 = d2;
+    }
+
+    void MD1() {
+        cout << "Método MD1() chamado" << endl;
+    }
+
+    void MD2() {
+        cout << "Método MD2() chamado" << endl;
+    }
+};
